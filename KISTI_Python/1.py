558b1d44b3b982c2501c879d52f5e56fb1cc1e81
n09b <woon1120@gmail.com>
2020-06-02 23:45:45 +0900

    uploaded

---
+++ KISTI_Python/1.py
@@ -0,0 +1,2 @@
+list1=[[0 for cols in range(10)] for rows in range(10)]
+print(list1)
---
+++ KISTI_Python/proj.py
@@ -0,0 +1,48 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 18,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "[[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]\n"
+     ]
+    }
+   ],
+   "source": [
+    "list1=[[0 for cols in range(10)] for rows in range(10)]\n",
+    "print(list1)\n",
+    "\n",
+    "print(list1)\n",
+    "#인덱스 [-1] 이용하여 못 가게 한다!\n",
+    "#t의 인덱스값은 어디로 정할지\n",
+    "#벽을 만났을때 가만히 있을지, 다시 입력 받을지 \n"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
